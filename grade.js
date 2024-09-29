01cc0e2a1f77016d48bcb24283c9599078de02e3
hamdiabdiaden <diagodu198@gmail.com>
2024-09-29 08:37:12 +0300

    grade j.s files

---
+++ grade.js
@@ -0,0 +1,22 @@
+let grade =100
+if (grade>=0 && grade<=40) 
+console.log("your grade is E")
+
+else if (grade>=41 && grade<=49) {
+    console.log("your grade is D")
+}
+else if (grade>=50 && grade<=59) {
+    console.log("your grade is C")
+}
+else if (grade>=60 && grade<=69){
+    console.log("your grade is B")
+}
+else if(grade>=70 && grade<=79){
+    console.log("your grade is A")
+}
+else {
+    console.log("invalid input")
+}
+
+
+
